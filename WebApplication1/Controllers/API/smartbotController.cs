1b0d09d084c57d0c0476e62a2aa62d562ad8f6bd
Manish Jha <manishjha.master@gmail.com>
2017-07-18 15:38:45 +0530

    Add project files.

---
+++ WebApplication1/Controllers/API/smartbotController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication1.Controllers.API
+{
+    [EnableCors("*", "*", "*")]
+    public class smartbotController : ApiController
+    {
+        [HttpPost]
+        public string GetStringfromBot(string inputstring)
+        {
+            return inputstring+2;
+        }
+    }
+}
---
+++ WebApplication2/JavaScript.js
@@ -0,0 +1,33 @@
+﻿var submitbtn = document.querySelector('#botsubmit');
+var inputstr = document.querySelector('#inputbox');
+var list = document.querySelector('#list')
+
+submitbtn.addEventListener('click', abc);
+
+function abc() {
+    var http = new XMLHttpRequest();
+    var url = 'http://localhost:57929/api/smartbot/GetStringfromBot?inputstring=' + inputstr.value;
+    var method = 'POST';
+    http.open(method, url);
+    var listItemQuestion = document.createElement('li');
+    listItemQuestion.textContent = inputstr.value;
+    list.appendChild(listItemQuestion);
+    http.onreadystatechange = function () {
+        if (http.readyState === XMLHttpRequest.DONE && http.status === 200){
+            var listItemAnswer = document.createElement('li');
+            listItemAnswer.textContent = http.response;
+            list.appendChild(listItemAnswer);
+        }
+        else if (http.readyState === XMLHttpRequest.UNSENT && http.status !== 200) {
+            var listItemAnswer = document.createElement('li');
+            listItemAnswer.textContent = "Sorry try again";
+            list.appendChild(listItemAnswer);
+        }
+
+    }
+   
+       
+
+    console.log(http);
+    http.send();
+}
